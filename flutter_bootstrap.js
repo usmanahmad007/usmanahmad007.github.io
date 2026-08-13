82b18bf7d5047cf5116fc5bafa7871fd66d86f61
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-08-14 02:20:42 +0500

    Add AI Model Loader screen and transparent glassmorphic Certificate cards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4002518373"
+    serviceWorkerVersion: "2874123444"
   }
 });
