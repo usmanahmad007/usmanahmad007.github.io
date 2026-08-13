8f9d697c6b414fc1240761fb2378ce193bdbb58a
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-08-13 20:54:11 +0500

    Add 8 verified certificates with category filtering and Google Drive buttons

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1630454805"
+    serviceWorkerVersion: "10743450"
   }
 });
