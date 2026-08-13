a303863b85f5e9fe8ca384b312e45e3595f73b3e
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-08-13 21:08:59 +0500

    Redesign Project Cards & Certificate Cards into compact high-density grids to eliminate long vertical scrolling

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3998318465"
+    serviceWorkerVersion: "4002518373"
   }
 });
