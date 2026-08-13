247caa7b7048b1f59a3d23021ce1aa1c6e61519f
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-08-14 02:48:00 +0500

    Add Cyber Robot Mascot synced to scroll direction and update typography

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3221306226"
+    serviceWorkerVersion: "2077375377"
   }
 });
