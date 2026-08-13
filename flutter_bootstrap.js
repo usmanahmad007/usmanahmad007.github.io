7f533ae8a84a8794b8c2c4487fc703bdf5f3bac4
APPLE <apple@APPLEs-MacBook-Pro.local>
2026-08-13 15:10:07 +0500

    Add Random OTP Email Generator & Verification System

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "718830305"
+    serviceWorkerVersion: "561850059"
   }
 });
